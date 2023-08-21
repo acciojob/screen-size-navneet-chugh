d881e11b7a1071651da00f6f69bd09b3a310e4d0
navneet-chugh <navneetchugh15@gmail.com>
2023-08-21 21:36:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function updateSize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const sizeText = `Width: ${width} and Height: ${height}`;
+  document.getElementById('sizeText').textContent = sizeText;
+}
+
+// Initial call
+updateSize();
+
+// Listen for window resize events
+window.addEventListener('resize', updateSize);
